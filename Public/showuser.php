0c5e4cc51fdd0823f6f13805653f22e6291e7b98
gosowski <osowski.grzegorz@gmail.com>
2017-11-03 12:35:57 +0100

    Added displaying user tweets

--- Public/showuser.php
+++ Public/showuser.php
@@ -1,10 +1,49 @@
+<?php
+
+session_start();
+
+require_once (__DIR__ . './../sql/config.php');
+require_once (__DIR__.'./../Model/User.php');
+require_once (__DIR__.'./../Model/Tweet.php');
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <meta lang="pl">
     <meta charset="utf8">
+    <link href="./../CSS/span.css" type="text/css" rel="stylesheet">
 </head>
 <body>
+<div class="rectangle">
+    <?php
+
+    $tweets = Tweet::loadAllTweetsByUserId($conn, $_SESSION['id']);
+    if($tweets) {
+        foreach($tweets as $tweet) {
+            $id = $tweet -> getId();
+            $text = $tweet -> getText();
+            $creationDate = $tweet -> getCreationDate();
+            echo "<div class='tweet'>";
+            echo "Utworzony: ".$creationDate."<br>";
+            echo $text;
+            echo "<div>";
+            echo '<a href="showtweet.php?id='.$id.'">'.'Pokaż</a>';
+            echo '</div>';
+            echo "</div>";
+        }
+    } else {
+        echo "<span class='error'>Nie napisałeś jeszcze żadnych tweetów</span>";
+    }
+    ?>
+</div>
+<div class="linki">
+
+    <a href="logout.php">Wyloguj się</a><br>
+    <a href="index.php">Powrót do strony głównej</a><br>
+    <a href="modifyuser.php">Mój profil</a>
+</div>
 
 </body>
 </html>
