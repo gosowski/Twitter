cf170a7c670b1d9068a1b08240abedaaaccc0560
gosowski <osowski.grzegorz@gmail.com>
2017-11-03 11:00:18 +0100

    Tweet class done

--- Model/Tweet.php
+++ Model/Tweet.php
@@ -4,17 +4,15 @@ class Tweet
 {
     private $id;
     private $userId;
-    private $text = 'text';
-    private $creationDate = '20170605 172345';
+    private $text;
+    private $creationDate;
 
     public function __construct()
     {
-        $this -> id = -1;
+        $this -> id = null;
         $this -> userId = 0;
         $this -> text = '';
         $this -> creationDate = '';
-
-
     }
 
     public function getId()
@@ -23,12 +21,8 @@ class Tweet
     }
 
     public function setUserId($newUserId) {
-        if(is_integer($newUserId)) {
             $this -> userId = $newUserId;
             return $this;
-        } else {
-            return "Błędny user_id!";
-        }
     }
 
     public function getUserId () {
@@ -36,12 +30,8 @@ class Tweet
     }
 
     public function setText ($newText) {
-        if(strlen($newText) <= 140) {
             $this -> text = $newText;
             return $this;
-        } else {
-            return "Twój wpis jest zbyt długi! Maksymalna długość tekstu to 140znaków.";
-        }
     }
 
     public function getText () {
@@ -49,43 +39,94 @@ class Tweet
     }
 
     public function setCreationDate ($newCreationDate) {
-        $setDate = date('Y.m.d H:i:s', strtotime($newCreationDate));
-        $this -> creationDate = $setDate;
+        $this -> creationDate = $newCreationDate;
+        return $this;
     }
 
     public function getCreationDate () {
         return $this -> creationDate;
     }
 
+    static public function loadTweetById(PDO $conn, $id) {
+        $stmt = $conn->prepare('SELECT * FROM tweets WHERE id=:id');
+        $res = $stmt->execute(['id'=>$id]);
+        if($res && $stmt->rowCount() > 0) {
+            $row = $stmt->fetch();
+            $tweet = new Tweet();
+            $tweet->id = $row["id"];
+            $tweet->setUserId($row["user_id"]);
+            $tweet -> setText($row['text']);
+            $tweet -> setCreationDate($row['creationDate']);
+
+            return $tweet;
+        }
+    }
+
     static public function loadAllTweetsByUserId(PDO $conn,	$user_id) {
         $res = [];
-        $stmt =	$conn->prepare('SELECT	`text` FROM tweets WHERE	user_id=:user_id');
+        $stmt =	$conn->prepare('SELECT	* FROM tweets WHERE user_id=:user_id');
         $result	= $stmt->execute(['user_id' => $user_id]);
         if	($result === true && $stmt->rowCount() > 0)	{
             foreach($stmt as $row) {
-                $res[] = $row['text'];
+                $tweet = new Tweet();
+                $tweet -> id = $row['id'];
+                $tweet -> setUserId($row['user_id']);
+                $tweet -> setText($row['text']);
+                $tweet -> setCreationDate($row['creationDate']);
+                $res[] = $tweet;
             }
             return $res;
         }
     }
 
     static public function loadAllTweets (PDO $conn) {
-        $res =[];
-        $sql = 'SELECT text FROM tweets';
-        $stmt = $conn -> query($sql);
-        foreach ($stmt as $row) {
-            $res[] = $row['text'];
+        $stmt = $conn->query('SELECT * FROM tweets ORDER BY creationDate DESC');
+        $res = [];
+        foreach ($stmt->fetchAll() as $row) {
+            $tweet = new Tweet();
+            $tweet->id = $row["id"];
+            $tweet->setUserId($row["user_id"]);
+            $tweet -> setText($row['text']);
+            $tweet -> setCreationDate($row['creationDate']);
+            $res[] = $tweet;
         }
         return $res;
     }
 
-    public function saveToDB () {
+    public function saveToDB (PDO $conn) {
+        if(!$this->getId()) {
+            $stmt = $conn->prepare(
+                'INSERT INTO tweets (user_id, text, creationDate) VALUES (:user_id, :text, NOW())'
+            );
+            $res = $stmt->execute([
+                'user_id' => $this->getUserId(),
+                'text' => $this->getText()
+            ]);
+            if($res !== false) {
+                $this->id = $conn->lastInsertId();
+                return true;
+            }
+        } else {
+            $stmt =$conn->prepare(
+                'UPDATE tweets SET user_id=:user_id, text=:text, creationDate=NOW() WHERE id=:id'
+            );
+            $res = $stmt->execute([
+                'user_id' => $this->getUserId(),
+                'text' => $this->getText(),
+                'id' => $this->getId()
+            ]);
+            return (bool) $res;
+        }
+        return false;
 
     }
 
+
 }
-$conn = new PDO("mysql:host=localhost; dbname=twitter; charset=utf8", "root", "coderslab" );
-$conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//$conn = new PDO("mysql:host=localhost; dbname=twitter; charset=utf8", "root", "coderslab" );
+//$conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
 
 
 
